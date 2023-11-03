28546d1cbd989e783419467ac0b867a772c61a56
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2023-11-03 17:25:51 -0600

    Add files via upload

---
+++ Tarea Corta/MergeSort.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+
+class HelloWorld {
+
+    public static void merge(
+
+  int[] array, int[] new_array_1, int[] new_array_2, int left, int right) {
+   // defining parameters
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < left && j < right) {  // conditions for merging
+
+        if (new_array_1[i] <= new_array_2[j]) {
+            array[k++] = new_array_1[i++];
+        }
+        else {
+            array[k++] = new_array_2[j++];
+        }
+    }
+
+    while (i < left) {
+        array[k++] = new_array_1[i++];
+    }
+
+    while (j < right) {
+        array[k++] = new_array_2[j++];
+    }
+}
+
+    public static void mergeSort(int[] array, int length) { /** required parameters */
+	if (length < 2) {  //condition for the length of array
+    	return;
+	}
+
+	int middle = length / 2;  // defining new parameter middle
+
+	int [ ] new_array_1 = new int [middle]; /** defining the new first array after division */
+	int [ ] new_array_2 = new int [length - middle]; /** defining the new second array */
+
+
+	for (int i = 0; i < middle; i++) { /**applying condition for sorting of new array 1 */
+    	new_array_1 [ i ] = array [ i ];
+	}
+
+	for (int i = middle; i < length ; i++) { /**applying condition for sorting of new array 2 */
+    	new_array_2 [ i - middle] = array [ i ];
+	}
+
+	mergeSort (new_array_1, middle); /** calling merge sort function for new array 1 */
+	mergeSort (new_array_2, length - middle); /** calling merge sort function for new array 2 */
+
+
+	merge(array, new_array_1, new_array_2, middle, length - middle); /** calling function for merging of new array 1 and new array 2 */
+}
+
+
+    public static void main(String[] args) {
+        int[] testSizes = {10000, 100000, 1000000};
+
+        for (int size : testSizes) {
+            int[] testScores = new int[size];
+
+            // Llenar el arreglo con valores aleatorios para cada prueba
+            for (int i = 0; i < size; i++) {
+                testScores[i] = (int) (Math.random() * 1000);
+            }
+
+            long startTime = System.nanoTime();
+            mergeSort(testScores, size);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime) / 1000000; // Duración en milisegundos
+
+            System.out.println("Tiempo de ejecución para " + size + " elementos: " + duration + " milisegundos");
+        }
+    }
+}
---
+++ Tarea Corta/QuickSort.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+class QuickSortBenchmark {
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] testSizes = {10000, 100000, 1000000};
+
+        for (int size : testSizes) {
+            int[] testScores = new int[size];
+
+            // Llenar el arreglo con valores aleatorios para cada prueba
+            for (int i = 0; i < size; i++) {
+                testScores[i] = (int) (Math.random() * 1000);
+            }
+
+            long startTime = System.nanoTime();
+            quickSort(testScores, 0, size - 1);
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime) / 1000000; // Duración en milisegundos
+
+            System.out.println("Tiempo de ejecución para " + size + " elementos: " + duration + " milisegundos");
+        }
+    }
+}
+
+
+// This code is contributed by Ayush Choudhary
+// Improved by Ajay Virmoti
